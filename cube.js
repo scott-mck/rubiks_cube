c10cfe6b5eac6c3641cd383327a5d44170ad590c
Scott McKenzie <slammayjammay@gmail.com>
2015-06-23 19:46:33 -0700

    Write Cube#rotate clockwise and #rotateCounterClockwise

--- cube.js
+++ cube.js
@@ -38,26 +38,6 @@
            ['W', 'W', 'W']];
   };
 
-  Cube.prototype.u = function() {
-    var topRow = this.front[0];
-    this.front[0] = this.right[0];
-    this.right[0] = this.back[0];
-    this.back[0] = this.left[0];
-    this.left[0] = topRow;
-
-    // this.rotateClockwise(this.up);
-  };
-
-  Cube.prototype.uPrime = function() {
-    var topRow = this.front[0];
-    this.front[0] = this.left[0];
-    this.left[0] = this.back[0];
-    this.back[0] = this.right[0];
-    this.right[0] = topRow;
-
-    this.rotateCounterClockwise(this.up);
-  };
-
   Cube.prototype.r = function() {
     var rightRow = [this.front[0][2], this.front[1][2], this.front[2][2]]
     this.front[0][2] = this.down[0][2];
@@ -76,6 +56,57 @@
     this.up[1][2] = rightRow[1];
     this.up[2][2] = rightRow[2];
 
-    // this.rotateClockwise(this.right);
+    this.rotateClockwise(this.right);
   };
+
+  Cube.prototype.rotateClockwise = function(face) {
+    var top = face[0];
+    face[0] = [ face[0][2], face[0][1], face[0][0] ];
+
+    face[0][2] = face[2][2];
+    face[0][1] = face[2][1];
+    face[0][0] = face[2][0];
+
+    face[2] = [ face[0][2], face[1][2], face[2][2] ];
+
+    face[0][2] = top[0];
+    face[1][2] = top[1];
+    face[2][2] = top[2];
+  };
+
+  Cube.prototype.rotateCounterClockwise = function(face) {
+    var top = face[0];
+    face[0] = [ face[0][2], face[1][2], face[2][2] ];
+
+    face[0][2] = face[2][2];
+    face[1][2] = face[2][1];
+    face[2][2] = face[2][0];
+
+    face[2] = [ face[0][0], face[1][0], face[2][0] ];
+
+    face[0][0] = top[2];
+    face[0][1] = top[1];
+    face[0][2] = top[0];
+  };
+
+  Cube.prototype.u = function() {
+    var topRow = this.front[0];
+    this.front[0] = this.right[0];
+    this.right[0] = this.back[0];
+    this.back[0] = this.left[0];
+    this.left[0] = topRow;
+
+    this.rotateClockwise(this.up);
+  };
+
+  Cube.prototype.uPrime = function() {
+    var topRow = this.front[0];
+    this.front[0] = this.left[0];
+    this.left[0] = this.back[0];
+    this.back[0] = this.right[0];
+    this.right[0] = topRow;
+
+    this.rotateCounterClockwise(this.up);
+  };
+
 })();
