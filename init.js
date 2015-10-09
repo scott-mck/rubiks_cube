da7aebdb17d49a777d01be778edec441457cd55b
Scott McKenzie <slammayjammay@gmail.com>
2015-10-09 01:21:04 -0700

    Render 4x4

--- init.js
+++ init.js
@@ -14,7 +14,7 @@
     scene = new THREE.Scene();
 
     // Make individual cube geometry
-    for (var j = 0; j < 9; j++) {
+    for (var j = 0; j < 16; j++) {
       var geometry = new THREE.BoxGeometry( 100, 100, 100 );
 
       // Color each individual cube:
@@ -38,7 +38,7 @@
       geometry.faces[11].color.setRGB(0, 1, 0);
 
       // Create 3 mini-cubes and position along z-axis
-      for (var k = 0; k < 3; k++) {
+      for (var k = 0; k < 4; k++) {
         var material = new THREE.MeshBasicMaterial({
           color: 0xffffff,
           vertexColors: THREE.FaceColors
@@ -46,8 +46,8 @@
 
         var mesh = new THREE.Mesh(geometry, material);
         mesh.position.set(
-          103 -103 * ~~(j / 3),
-          103 -103 * (j % 3),
+          103 -103 * ~~(j / 4),
+          103 -103 * (j % 4),
           103 - 103 * k
         );
         mesh.name = "cubie"; // easy for id-ing when raycasting
@@ -64,7 +64,7 @@
 
     camera.position.x += 250;
     camera.position.y += 300;
-    camera.position.z -= 100;
+    camera.position.z += 100;
     camera.lookAt(cubes[13].position); // look at middle of Rubik's Cube
 
     rubiksCube = new Game.Cube(scene, camera, cubes);
