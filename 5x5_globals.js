f12d8fcc2b6aad67d56faf137a780f28cee9fa09
Scott McKenzie <slammayjammay@gmail.com>
2015-10-09 21:57:26 -0700

    Does 5x5 work easily

---
+++ 5x5_globals.js
@@ -0,0 +1,16 @@
+(function () {
+  cubieSize = 60;
+  cubieOffset = 2;
+  cubeDimensions = 5;
+  cubeStartPos = (4/2) * (cubieSize + cubieOffset);
+  cameraX = 250;
+  cameraY = 300;
+  cameraZ = -100;
+
+  doubleInclusive = true;
+  sizeSpecificScrambles = [
+    'rDouble', 'rDoublePrime', 'lDouble', 'lDoublePrime',
+    'right', 'left', 'up', 'down'
+  ];
+  scrambleLength = 70;
+})();
