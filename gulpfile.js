e831e9c1e4d748f5867e2d599f673c9abb725df7
Scott McKenzie <slammayjammay@gmail.com>
2016-07-24 01:47:31 -0700

    Ugh sass scss css ughgasdhglasdjhg

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 var cleancss = require('gulp-clean-css');
 
 gulp.task('build', function() {
-  console.log('gulping')
-  gulp.src('dist/built.js')
+  gulp.src('./dist/built.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('./dist/'));
 
-  gulp.src('dist/built.scss')
+  gulp.src('./dist/built.css')
     .pipe(cleancss())
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('default', ['build']);
