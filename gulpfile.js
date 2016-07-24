7ce1c545b81f9e5917f243f79a06f5b13b016b8e
Scott McKenzie <slammayjammay@gmail.com>
2016-07-24 01:41:56 -0700

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var uglify = require('gulp-uglify');
 var cleancss = require('gulp-clean-css');
 
 gulp.task('build', function() {
+  console.log('gulping')
   gulp.src('dist/built.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist/'));
