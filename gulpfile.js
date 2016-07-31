797e611cdbc4cc7f5542542edb147c6c47130a88
Scott McKenzie <slammayjammay@gmail.com>
2016-07-30 19:51:06 -0700

    node_modules/

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,38 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
+var del = require('del');
 var cleancss = require('gulp-clean-css');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 var browserify = require('gulp-browserify');
 var sourcemaps = require('gulp-sourcemaps');
 
+// Clean
+gulp.task('clean', function() {
+  del('dist/*');
+});
+
 // JavaScript
 gulp.task('js', function () {
+  gulp.src(['./src/js/*.js'])
+    .pipe(concat('built.js'))
+    .pipe(browserify())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/'));
+});
+
+// JavaScript Sourcemaps
+gulp.task('js-sourcemaps', function () {
   return gulp.src(['./src/js/*.js'])
     .pipe(sourcemaps.init())
-    .pipe(concat('./built.js'))
+    .pipe(concat('built.js'))
     .pipe(browserify())
-    .pipe(uglify({ magle: false }))
+    .pipe(uglify())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('dist/'));
 });
+
 
 // Sass
 gulp.task('sass', function() {
@@ -25,7 +41,7 @@ gulp.task('sass', function() {
     .pipe(sass())
     .pipe(cleancss())
     .pipe(rename('built.css'))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('dist/'));
 });
 
 // Watch
@@ -34,5 +50,5 @@ gulp.task('watch', function() {
   gulp.watch('./src/sass/*.scss', ['sass']);
 });
 
-gulp.task('default', ['js', 'sass']);
-gulp.task('dev', ['js', 'sass', 'watch']);
+gulp.task('default', ['clean', 'js', 'sass']);
+gulp.task('dev', ['clean', 'js-sourcemaps', 'sass', 'watch']);
