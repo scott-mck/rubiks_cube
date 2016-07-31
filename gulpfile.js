be7ebb89a7d1949093ebd85547f5a24d5c655dd0
Scott McKenzie <slammayjammay@gmail.com>
2016-07-30 17:33:06 -0700

    Browserify and watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,35 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var cleancss = require('gulp-clean-css');
+var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var browserify = require('gulp-browserify');
 
-gulp.task('build', function() {
-  gulp.src('dist/built.js')
+// JavaScript
+gulp.task('js', function () {
+  return gulp.src(['./src/js/*.js'])
+    .pipe(concat('./built.js'))
+    .pipe(browserify())
     .pipe(uglify())
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('./dist/'));
+});
 
-  gulp.src('dist/built.css')
+// Sass
+gulp.task('sass', function() {
+  gulp.src('./src/sass/*.scss')
+    .pipe(sass())
     .pipe(cleancss())
-    .pipe(gulp.dest('dist/'));
+    .pipe(rename('built.css'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+// Watch
+gulp.task('watch', function() {
+  gulp.watch('./src/js/*.js', ['js']);
+  gulp.watch('./src/sass/*.scss', ['sass']);
 });
 
 gulp.task('default', ['build']);
+gulp.task('dev', ['js', 'sass', 'watch']);
