c17329658dc67d5a63cf383bf9b609303f964073
Scott McKenzie <slammayjammay@gmail.com>
2016-09-16 12:49:12 -0700

    Move map

--- src/js/key-map.js
+++ src/js/key-map.js
@@ -1,10 +1,23 @@
 class KeyMap {
   constructor() {
-
+    this.keyMap = {
+      i: 'r',
+      k: 'rPrime',
+      j: 'u',
+      f: 'uPrime',
+      h: 'f',
+      g: 'fPrime',
+      s: 'd',
+      l: 'dPrime',
+      e: 'l',
+      d: 'lPrime',
+      q: 'b',
+      p: 'bPrime'
+    }
   }
 
   getNotation(letter) {
-    
+    return this.keyMap[letter]
   }
 }
 
