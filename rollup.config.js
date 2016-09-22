edd04ea44b4ff097439b5c69e210aef3ad371cc6
Scott McKenzie <slammayjammay@gmail.com>
2016-09-21 19:22:28 -0700

    CommonJS for modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,25 @@
 import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   entry: './src/js/main.js',
   plugins: [
     babel({
       exclude: 'node_modules/**'
+    }),
+    resolve({
+      main: true
+    }),
+    commonjs({
+      include: 'node_modules/**'
     })
   ],
   targets: [
     {
       dest: 'dist/built.js',
       moduleName: 'built.js',
-      format: 'umd'
+      format: 'iife'
     }
-  ],
-  globals: {
-    three: 'THREE',
-    jquery: '$',
-    gsap: 'TweenMax'
-  }
+  ]
 }
