60c6c243dc2b0390223ad32dc7f6ab992a8dbdd5
Scott McKenzie <slammayjammay@gmail.com>
2016-10-14 23:10:02 -0700

    Add in config file for rollup server plugins

---
+++ rollup-server.config.js
@@ -0,0 +1,32 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import server from 'rollup-plugin-serve'
+import liveReload from 'rollup-plugin-livereload'
+
+export default {
+  entry: './src/js/main.js',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      externalHelpers: true,
+      runtimeHelpers: true
+    }),
+    resolve({
+      main: true
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    server({
+      port: 8080
+    }),
+    liveReload({
+      watch: 'dist'
+    })
+  ],
+  targets: [{
+    dest: 'dist/built.js',
+    format: 'iife'
+  }]
+}
