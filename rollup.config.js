d2aa5ad4028ec985956e93f95a13063a819bb14e
Scott McKenzie <slammayjammay@gmail.com>
2016-10-14 23:27:15 -0700

    Include server plugins based on cli args

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,37 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
+import server from 'rollup-plugin-serve'
+import liveReload from 'rollup-plugin-livereload'
+
+var plugins = [
+  babel({
+    exclude: 'node_modules/**',
+    externalHelpers: true,
+    runtimeHelpers: true
+  }),
+  resolve({
+    main: true
+  }),
+  commonjs({
+    include: 'node_modules/**'
+  })
+]
+
+if (process.argv.indexOf('--serve') > -1) {
+  plugins.push(
+    server({
+      port: 8080
+    }),
+    liveReload({
+      watch: 'dist'
+    })
+  )
+}
 
 export default {
   entry: './src/js/main.js',
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-      externalHelpers: true,
-      runtimeHelpers: true
-    }),
-    resolve({
-      main: true
-    }),
-    commonjs({
-      include: 'node_modules/**'
-    })
-  ],
+  plugins: plugins,
   targets: [{
     dest: 'dist/built.js',
     format: 'iife'
