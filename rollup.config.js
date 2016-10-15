e3b9847e8f62039c16875d425dfb897f35f19c2d
Scott McKenzie <slammayjammay@gmail.com>
2016-10-14 21:50:34 -0700

    Live reloading\!

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 import server from 'rollup-plugin-serve'
+import liveReload from 'rollup-plugin-livereload'
 
 export default {
   entry: './src/js/main.js',
@@ -19,6 +20,9 @@ export default {
     }),
     server({
       port: 8080
+    }),
+    liveReload({
+      watch: 'dist'
     })
   ]
 }
