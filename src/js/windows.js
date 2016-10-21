c4a58d43c17b511a27c7fdc59cd12e2e2bd8ae7d
Scott McKenzie <slammayjammay@gmail.com>
2016-10-21 00:00:11 -0700

    Add windows for debugging

---
+++ src/js/windows.js
@@ -0,0 +1,18 @@
+import THREE from 'three'
+import rubiksCube from './rubiks-cube'
+import animator from './animator'
+import grabber from './grabber'
+import g from './globals'
+import scene from './scene'
+import timer from './timer'
+import renderer from './renderer'
+
+window.THREE = THREE
+window.rubiksCube = rubiksCube
+window.animator = animator
+window.grabber = grabber
+window.g = g
+window.scene = scene
+window.timer = timer
+window.renderer = renderer
+window.animator = animator
