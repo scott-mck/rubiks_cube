3931c049f317d00bc388dace5a2c261f10b66a5f
Scott McKenzie <slammayjammay@gmail.com>
2016-10-20 18:09:37 -0700

    Add color util

---
+++ src/js/utils/color.js
@@ -0,0 +1,8 @@
+export const color = ({ r, g, b }) => {
+  if (r === 1 && g === 1 && b === 1) return 'white'
+  if (r === 1 && g === 1) return 'yellow'
+  if (r === 1 && g === 0.5) return 'orange'
+  if (r === 1) return 'red'
+  if (g === 1) return 'green'
+  if (b === 1) return 'blue'
+}
