1c3be5817d55301506c21a5020293ec8b57e27cd
Scott McKenzie <slammayjammay@gmail.com>
2016-10-22 22:47:51 -0700

    Remove unnecessary code

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,4 @@
 import './windows'
-
 import EventHandler from './event-handler'
-import Solver from './solver'
 
 window.addEventListener('load', () => EventHandler.init())
-
-window.solver = new Solver()
