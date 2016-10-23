755eda541bd08355d477feffad90a1a2b1c1974f
Scott McKenzie <slammayjammay@gmail.com>
2016-10-23 13:13:08 -0700

    Add f2l solver file

--- src/js/solver/index.js
+++ src/js/solver/index.js
@@ -1,10 +1,20 @@
 import crossSolver from './cross-solver'
+import F2LSolver from './f2l-solver'
 
 class Solver {
 	constructor() {}
 
 	solve() {
+		this.solveCross()
+		this.solveF2L()
+	}
+
+	solveCross() {
 		crossSolver.solve()
+	}
+
+	solveF2L() {
+		F2LSolver.solve()
 	}
 }
 
