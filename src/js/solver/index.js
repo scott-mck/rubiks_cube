a0423f5c91aa79713d5fe6e9073149e51a1fa222
Scott McKenzie <slammayjammay@gmail.com>
2016-11-09 21:39:11 -0800

    Add oll step in solving

--- src/js/solver/index.js
+++ src/js/solver/index.js
@@ -1,5 +1,6 @@
 import crossSolver from './cross-solver'
 import F2LSolver from './f2l-solver'
+import ollSolver from './oll-solver'
 
 class Solver {
 	constructor() {}
@@ -7,6 +8,7 @@ class Solver {
 	async solve() {
 		await this.solveCross()
 		await this.solveF2L()
+		await this.solveOll()
 	}
 
 	solveCross() {
@@ -15,6 +17,10 @@ class Solver {
 
 	solveF2L() {
 		return F2LSolver.solve()
+	}
+
+	solveOll() {
+		return ollSolver.solve()
 	}
 }
 
